673bc15eafc327021ea3494d36ba43fea65054bf
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-03-03 09:57:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const header = document.getElementById("h1");
+header.textContent = "Simple Form";
+
+
+function showMessage(){
+    alert("You Logged in");
+}
+
+const header2 = document.getElementById("h1");
+header2.style.color = "green";
+
+
+
+
+
+
+
+
 
 
 
